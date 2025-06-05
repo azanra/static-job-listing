14373d581681b5e34a7c8e54d255f3f754175996
azan-personal <112457939+azanra@users.noreply.github.com>
2025-06-05 08:14:12 +0700

    Add selected filter component for each filter

--- src/component/searchBar.jsx
+++ src/component/searchBar.jsx
@@ -4,18 +4,28 @@ export default function SearchBar({ selectedFilter }) {
   const list = filterValue.map((filter, index) => {
     if (Array.isArray(filter)) {
       const listArr = filter.map((item, i) => {
-        return <li key={`item-${i}`}>{item}</li>;
+        return <SelectedFilter key={`item-${i}`} filter={item} />;
       });
       return listArr;
     } else if (filter) {
-      return <li key={index}>{filter}</li>;
+      return <SelectedFilter key={index} filter={filter} />;
     }
   });
+
   return (
     <>
       <div>
         <ul>{list}</ul>
+        <button>Clear</button>
       </div>
     </>
+  );
+}
+
+function SelectedFilter({ filter }) {
+  return (
+    <div>
+      {filter} <button>X</button>
+    </div>
   );
 }
