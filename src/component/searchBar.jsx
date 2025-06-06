a0221aef157ef7d41fedc0f7053cfbe7e3c3095c
azan-personal <112457939+azanra@users.noreply.github.com>
2025-06-06 11:37:36 +0700

    Dispatch remove event on selected filter click

--- src/component/searchBar.jsx
+++ src/component/searchBar.jsx
@@ -1,14 +1,18 @@
-export default function SearchBar({ selectedFilter }) {
+import { useContext } from "react";
+import { DispatchContext, SelectedFilterContext } from "../selectedFilter";
+
+export default function SearchBar() {
+  const selectedFilter = useContext(SelectedFilterContext);
   const filterValue = Object.values(selectedFilter);
 
   const list = filterValue.map((filter, index) => {
     if (Array.isArray(filter)) {
       const listArr = filter.map((item, i) => {
-        return <SelectedFilter key={`item-${i}`} filter={item} />;
+        return <SelectedFilter key={`item-${i}`} value={item} />;
       });
       return listArr;
     } else if (filter) {
-      return <SelectedFilter key={index} filter={filter} />;
+      return <SelectedFilter key={index} value={filter} />;
     }
   });
 
@@ -22,10 +26,22 @@ export default function SearchBar({ selectedFilter }) {
   );
 }
 
-function SelectedFilter({ filter }) {
+function SelectedFilter({ type, value }) {
+  const dispatch = useContext(DispatchContext);
   return (
     <div>
-      {filter} <button>X</button>
+      <span>{value}</span>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "remove",
+            filterType: type,
+            filterValue: value,
+          });
+        }}
+      >
+        X
+      </button>
     </div>
   );
 }
